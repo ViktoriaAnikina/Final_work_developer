d7c1b0945b6e8a32f5f09bf216aa28ce51890c00
letterssino <anikin.paul@yandex.ru>
2024-03-14 11:27:48 +0300

    Создание консольного приложение, добавление метода хранения тестовых массивов и метода Main

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace FinalWork
+{
+    public class Array
+    {
+        
+
+        public static string[] ArrayTest(int Number)
+        {
+            string[] Data = ["1","2"];
+
+            if (Number == 1)
+            {
+                
+                Data = ["“Hello”", "“2”", "“world”", "“:-)”"];
+            }
+            if (Number == 2) 
+            {
+                
+                Data = ["“1234”", "“1567”", "“-2”", "“computer science”"];
+            }
+            if (Number == 3)
+            {
+                
+                Data = ["“Russia”", "“Denmark”", "“Kazan”"];
+            }
+
+            return Data;
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine("[" +String.Join(", ", ArrayTest(1)) + "]");
+        }
+    }
+}
