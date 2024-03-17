45e1fe4ae6abad803210c2bc6f32c72426911792
Павел Аникин <anikin.paul@yandex.ru>
2024-03-17 22:32:53 +0300

    Тест

--- HelpMethods.cs
+++ HelpMethods.cs
@@ -5,6 +5,8 @@ namespace FinalWork
 {
     public class HelperMethods // Вспомогательный класс, хранящий вспомогательные методы.
     {
+        HelperDataArrayTests dataArray = new HelperDataArrayTests();
+
         public void ArrayPrint(string[] Array) // метод выполняющий отображение массива в терминале.
         {
             Console.WriteLine("[" + String.Join(", ", Array) + "]"); // склейка значений, для аналогичного вывода результата в условиях задания.
@@ -42,6 +44,97 @@ namespace FinalWork
                 return ArrayItog;
             }
             
+        }
+        public int CheckCondition(int i){
+            int Number2 = -0;
+            if (i == 1)
+            {
+                Console.WriteLine("Выберите вариант 1 или 2");
+                Number2 = Convert.ToInt32(Console.ReadLine());
+                if (Number2 != 1 & Number2 != 2)
+                {
+                    Thread.Sleep(400);
+                    CheckCondition(1);
+                }
+                return Number2;
+            }
+            if (i == 2)
+            {
+                Console.WriteLine("Выберите один из предложенных вариантов");
+                Number2 = Convert.ToInt32(Console.ReadLine());
+                if (Number2 != 1 && Number2 != 2 && Number2 != 3)
+                {
+                    Thread.Sleep(400);
+                    CheckCondition(2);
+                }
+                return Number2;
+            }
+            return Number2;
+        }
+
+        public int CheckOUT(int Number, int Size)
+        {
+            int Number0 = -0;
+            Console.WriteLine("Выберите один из предложенных вариантов");
+            Number0 = Convert.ToInt32(Console.ReadLine());
+            if (Number0 < 1 || Number0 > Size)
+            {
+                Console.WriteLine($"Выберите один вариант 1-{Size}");
+                CheckOUT(Number0,Size);
+            }
+            return Number0;
+        }
+
+        public string[] ArrayTest()
+        {
+            Console.WriteLine("Варианты массивов:");
+            Console.Write("1. ");
+            ArrayPrint(dataArray.ArrayTest(1));
+            Console.Write("2. ");
+            ArrayPrint(dataArray.ArrayTest(2));
+            Console.Write("3. ");
+            ArrayPrint(dataArray.ArrayTest(3));
+            //int ArrayNumber = CheckCondition(2);
+            int ArrayNumber = CheckOUT(2,3);
+            return dataArray.ArrayTest(ArrayNumber);
+        }
+
+        public string[] CreatArray()
+        {
+            Console.WriteLine("Введите количество элементов в массиве:");
+            int Length = 0;
+            for(;Length < 2;)
+            {
+                Length = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Значение количества элементов в массиве, должно быть больше 1");
+            }
+            //Length = Convert.ToInt32(Console.ReadLine());
+            string[] Array = new string[Length];
+            string ReadConsole;
+            for (int i = 0; i < Length; i++)
+            {
+                Console.WriteLine($"Введите {i+1} значение");
+                for(;;)
+                {
+                    #pragma warning disable CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
+                    ReadConsole = Console.ReadLine();
+                    #pragma warning restore CS8600 // Преобразование литерала, допускающего значение NULL или возможного значения NULL в тип, не допускающий значение NULL.
+                    if (ReadConsole != "")
+                    {
+                        #pragma warning disable CS8601 // Возможно, назначение-ссылка, допускающее значение NULL.
+                        Array[i] = ReadConsole;
+                        #pragma warning restore CS8601 // Возможно, назначение-ссылка, допускающее значение NULL.
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Введите значние отличное от NULL");
+                    }
+                }
+                
+            }
+            
+            return Array;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,8 +11,24 @@ namespace FinalWork
             HelperDataArrayTests dataArray = new HelperDataArrayTests();
             HelperMethods helpMethods = new HelperMethods();
 
+            string[] ArrayTest;
+
+            Console.Write("Выберите вариант начальных данных, где ");
+            Console.WriteLine("1 - Выбор из придложенных варианов, 2 - ввод массива чисел через консоль: ");
+            //int NumberCondition = helpMethods.CheckCondition(1);
+            int NumberCondition = helpMethods.CheckOUT(1,2);
+            if (NumberCondition == 1)
+            {
+                ArrayTest = helpMethods.ArrayTest();
+            }
+            else
+            {
+                ArrayTest = helpMethods.CreatArray();
+            }
+
+            
             // string[] ArrayTest = ["1","2"]; // Вариант для использования своего массива.
-            string[] ArrayTest = dataArray.ArrayTest(1); // В параметре выбрать один из 3 вариантов предложенных для проверки.
+            //string[] ArrayTest = dataArray.ArrayTest(1); // В параметре выбрать один из 3 вариантов предложенных для проверки.
             string[] ArrayItog = helpMethods.ArrayCheck(ArrayTest);  //Получения массива с выполнением условий задания.
             Console.Write("[" + String.Join(", ", ArrayTest) + "] → "); // склейка с добавлением начального массива в вывод.
             helpMethods.ArrayPrint(ArrayItog); // Печать полученного массива в терминал.
